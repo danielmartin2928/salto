77462285ebecd81ae0bd266a776f8af4e75440e7
danielmartin2928 <122784748+danielmartin2928@users.noreply.github.com>
2023-01-16 10:59:23 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    personaje_abajo.change(LedSpriteProperty.Y, -1)
+    personaje_arriba.change(LedSpriteProperty.Y, -1)
+    basic.pause(500)
+    personaje_abajo.change(LedSpriteProperty.Y, 1)
+    personaje_arriba.change(LedSpriteProperty.Y, 1)
+})
+let personaje_arriba: game.LedSprite = null
+let personaje_abajo: game.LedSprite = null
+personaje_abajo = game.createSprite(1, 4)
+personaje_arriba = game.createSprite(1, 3)
+let objeto = game.createSprite(4, 4)
+basic.forever(function () {
+    objeto.change(LedSpriteProperty.X, -1)
+    basic.pause(400)
+    if (objeto.get(LedSpriteProperty.X) == 0) {
+        objeto.change(LedSpriteProperty.X, 4)
+    }
+    if (objeto.isTouching(personaje_abajo)) {
+        game.gameOver()
+    }
+})
